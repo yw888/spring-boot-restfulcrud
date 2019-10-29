032f9b065fd594a8848daf5c22dd26fbedad5b77
wwvip <yw46324621?@>
2019-10-29 10:24:47 +0800

    '增加';

--- src/main/java/com/bjxrgz/springboot/config/MyMvcConfig.java
+++ src/main/java/com/bjxrgz/springboot/config/MyMvcConfig.java
@@ -6,18 +6,16 @@ import com.bjxrgz.springboot.component.LoginHandlerInterceptor;
 import com.bjxrgz.springboot.component.MyLocaleResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.LocaleResolver;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.*;
 
 
 @Configuration
-public class MyMvcConfig extends WebMvcConfigurationSupport{
+public class MyMvcConfig implements WebMvcConfigurer {
+
     @Override
-    protected void addViewControllers(ViewControllerRegistry registry) {
-//        super.addViewControllers(registry);
+    public void addViewControllers(ViewControllerRegistry registry) {
         //浏览器发送/ceshi,映射到success页面
         registry.addViewController("/ceshi").setViewName("success");
         registry.addViewController("/").setViewName("login");
@@ -26,24 +24,55 @@ public class MyMvcConfig extends WebMvcConfigurationSupport{
     }
 
     @Override
-    protected void addInterceptors(InterceptorRegistry registry) {
+    public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new LoginHandlerInterceptor()).addPathPatterns("/**").
                 excludePathPatterns("/index.html","/","/user/login","/webjars/**","/asserts/**");
     }
 
     @Override
-    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/**")
                 .addResourceLocations("classpath:/resources/")
                 .addResourceLocations("classpath:/static/")
                 .addResourceLocations("classpath:/public/");
         registry.addResourceHandler("/webjars/**")
                 .addResourceLocations("classpath:/META-INF/resources/webjars/");
-        super.addResourceHandlers(registry);
     }
 
     @Bean
     public LocaleResolver localeResolver(){//名字必须为localeResolver
         return new MyLocaleResolver();
     }
+//    WebMvcConfigurationSupport-->不需要返回逻辑视图,可以选择继承此类
+//    WebMvcCofigurer-->返回逻辑视图,可以选择实现此方法,重写addInterceptor方法
+//    下边是继承WebMvcConfigurationSupport写法
+
+    //    @Override
+//    protected void addViewControllers(ViewControllerRegistry registry) {
+////        super.addViewControllers(registry);
+//        //浏览器发送/ceshi,映射到success页面
+//        registry.addViewController("/ceshi").setViewName("success");
+//        registry.addViewController("/").setViewName("login");
+//        registry.addViewController("/index.html").setViewName("login");
+//        registry.addViewController("/main.html").setViewName("dashboard");
+//    }
+//
+//    @Override
+//    protected void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(new LoginHandlerInterceptor()).addPathPatterns("/**").
+//                excludePathPatterns("/index.html","/","/user/login","/webjars/**","/asserts/**");
+//    }
+//
+//    @Override
+//    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("/**")
+//                .addResourceLocations("classpath:/resources/")
+//                .addResourceLocations("classpath:/static/")
+//                .addResourceLocations("classpath:/public/");
+//        registry.addResourceHandler("/webjars/**")
+//                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+//        super.addResourceHandlers(registry);
+//    }
+//
+
 }
--- src/main/java/com/bjxrgz/springboot/controller/EmployeeController.java
+++ src/main/java/com/bjxrgz/springboot/controller/EmployeeController.java
@@ -1,11 +1,15 @@
 package com.bjxrgz.springboot.controller;
 
+import com.bjxrgz.springboot.dao.DepartmentDao;
 import com.bjxrgz.springboot.dao.EmployeeDao;
+import com.bjxrgz.springboot.entities.Department;
 import com.bjxrgz.springboot.entities.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Collection;
 
@@ -13,6 +17,10 @@ import java.util.Collection;
 public class EmployeeController {
     @Autowired
     EmployeeDao employeeDao;
+    
+    @Autowired
+    DepartmentDao departmentDao;
+    
     /**
      * 查询所欲员工列表
      * @return
@@ -22,5 +30,36 @@ public class EmployeeController {
         Collection<Employee> all = employeeDao.getAll();
         model.addAttribute("emps", all);
         return "emp/list";
+    }
+
+    /**
+     * 来到员工添加页面
+     * @return
+     */
+    @GetMapping(value = "/emp")
+    public String toAddPage(Model model){
+        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute("depts", departments);
+        return "emp/add";
+    }
+
+    /**
+     * 员工添加
+     * @return
+     */
+    @PostMapping(value = "/emp")
+    public String addEmp(Employee employee){
+        System.out.println("保存的员工信息:" + employee);
+        employeeDao.save(employee);
+        //redirect:表示重定向到页面 /代表当前项目路径
+        //forward: 表示转发到页面
+        return "redirect:/emps";
+    }
+
+    @GetMapping(value = "emp/{id}")
+    public String toEditPage(@PathVariable("id") Integer id, Model model){
+        Employee employee = employeeDao.get(id);
+        model.addAttribute("emp", employee);
+        return "emp/add";
     }
 }
